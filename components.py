7cdcdbfbb619df6b8d0377b54c6a6a57afd6bcc2
DmytroShvarts <92295322+DmytroShvarts@users.noreply.github.com>
2024-12-11 14:44:39 +0100

    added edit button

--- components.py
+++ components.py
@@ -3,14 +3,21 @@ import streamlit as st
 @st.dialog("Article details", width="large")
 def article_view(data):
     """Display article details in a dialog."""
+    startingbuttn = 0
     st.title(data['title'])
     st.write(data['content'])
     if data['label'] == "FAKE":
         st.error("This news is likely FAKE.")
+        startingbuttn = 0
     else:
         st.success("This news is likely REAL.")
-    if st.button(":material/gavel: Report"):
+        startingbuttn = 1
+    reportbtn = st.button(":material/gavel: Report")
+    if reportbtn:
         report()
+    editbtn = st.button(":material/edit: Edit")
+    if editbtn:
+        edit(startingbuttn)
 
 
 
@@ -25,3 +32,9 @@ def report():
     st.title("Report article:")
     st.text_area("Why is this article validation wrong?")
     st.button(":material/mail: Send report") #TODO: add code to submit report to database
+
+def edit(startingButton):
+    st.title("Edit article result:")
+    st.write("User report:")
+    st.write("I am absolutely appauled at the fact that this article's validation is incorrect, here are the specific reasons as to why this is wrong:Lorem ipsum dolor sit amet, ...")
+    st.radio("Validation result:",["Fake","True"],index=startingButton)
