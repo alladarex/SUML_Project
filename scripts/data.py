49cd57f81e6f4288c295786fbb08a83eca445895
DmytroShvarts <92295322+DmytroShvarts@users.noreply.github.com>
2025-01-20 06:11:36 +0100

    moved files to match main structure

--- scripts/data.py
+++ scripts/data.py
@@ -4,7 +4,7 @@ import streamlit as st
 @st.cache_data
 def load_data():
     # Load dataset
-    data = pd.read_csv("news.csv")
+    data = pd.read_csv("data/news.csv")
     # Ensure necessary columns are present
     required_columns = ['title', 'content', 'label']
     for col in required_columns:
